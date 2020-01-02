934730a4aa241b02457bf429e2682faa451af7a4
Vasic2000 <vasic2000@yandex.ru>
2020-01-02 17:18:20 +0300

    После ButterKnife

--- app/src/main/java/ru/vasic2000/my2/MainActivity.java
+++ app/src/main/java/ru/vasic2000/my2/MainActivity.java
@@ -3,39 +3,82 @@ package ru.vasic2000.my2;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.btnCounter1)
+    Button button1;
+    @BindView(R.id.btnCounter2)
+    Button button2;
+    @BindView(R.id.btnCounter3)
+    Button button3;
+
+    @BindView(R.id.tv1)
+    TextView tv1;
+    @BindView(R.id.tv2)
+    TextView tv2;
+    @BindView(R.id.tv3)
+    TextView tv3;
+
+    @BindView(R.id.progressBar)
+    ProgressBar pb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
 
-        Button button1 = findViewById(R.id.btnCounter1);
-        Button button2 = findViewById(R.id.btnCounter2);
-        Button button3 = findViewById(R.id.btnCounter3);
+    @OnClick(R.id.btnCounter1)
+    public void buttonOneClick() {
+        showLoading();
+        button1.setText("Crash 1!!!");
+        hideLoading();
+    }
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                button1.setText("Crash!!!");
-            }
-        });
+    @OnClick(R.id.btnCounter2)
+    public void buttonTwoClick() {
+        showLoading();
+        button2.setText("Crash 2!!!");
+        hideLoading();
+    }
+    @OnClick(R.id.btnCounter3)
+    public void buttonThreeClick() {
+        showLoading();
+        button3.setText("Crash 3!!!");
+        hideLoading();
+    }
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                button2.setText("Crash!!!");
-            }
-        });
+    private void showLoading() {
+        button1.setVisibility(View.INVISIBLE);
+        button2.setVisibility(View.INVISIBLE);
+        button3.setVisibility(View.INVISIBLE);
+        tv1.setVisibility(View.INVISIBLE);
+        tv2.setVisibility(View.INVISIBLE);
+        tv3.setVisibility(View.INVISIBLE);
+        pb.setVisibility(View.VISIBLE);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                button3.setText("Crash!!!");
-            }
-        });
+    private void hideLoading() {
+        button1.setVisibility(View.VISIBLE);
+        button2.setVisibility(View.VISIBLE);
+        button3.setVisibility(View.VISIBLE);
+        tv1.setVisibility(View.VISIBLE);
+        tv2.setVisibility(View.VISIBLE);
+        tv3.setVisibility(View.VISIBLE);
+        pb.setVisibility(View.INVISIBLE);
     }
 }
